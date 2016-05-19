5552825caee7cedb8258902cd99562fbb9dcde27
yuwei <shinyhz50817>
2016-05-19 16:34:09 +0800

    无缝滚动

--- js/myjs.js
+++ js/myjs.js
@@ -5,8 +5,9 @@
     //默认参数
     var defaults = {
         runeml: ".ad-list",
+        btn: '.btn',
         timer: 'timer',
-        gap: 2000,
+        gap: 1000,
         sum: 0,
         flg: true
     };
@@ -17,21 +18,28 @@
         //滚动
         var run = function () {
             $(options.runeml).stop().animate({
-                'margin-left': -$(options.runeml).find("li").outerWidth(true)
+                'margin-left': -$(options.runeml).find('li').outerWidth(true)
             }, 500, function () {
                 $(options.runeml).append($(options.runeml).find('li').first());
                 $(options.runeml).css({'margin-left': 0});
             });
 
         };
+        clearInterval(options.timer);
         //定时器
         options.timer = setInterval(run, options.gap);
 
         $(options.runeml).on('mouseover', function () {
             clearInterval(options.timer);
-        })
+        });
         $(options.runeml).on('mouseout', function () {
             options.timer = setInterval(run, options.gap);
-        })
+        });
+        $(options.btn).on('mouseover', function () {
+            clearInterval(options.timer);
+        });
+        $(options.btn).on('mouseout', function () {
+            options.timer = setInterval(run, options.gap);
+        });
     }
 })(jQuery)
