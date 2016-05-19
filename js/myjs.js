072cb458c31dd81fe106bc5c29ae74f2f5cf4164
yuwei <shinyhz50817>
2016-05-19 15:10:40 +0800

    无缝滚动
    无缝滚动-基础

---
+++ js/myjs.js
@@ -0,0 +1,37 @@
+/**
+ * Created by yuwei on 2016/5/13.
+ */
+(function ($) {
+    //默认参数
+    var defaults = {
+        runeml: ".ad-list",
+        timer: 'timer',
+        gap: 2000,
+        sum: 0,
+        flg: true
+    };
+    $.fn.roll = function (options) {
+        var options = $.extend(defaults, options);
+        $(options.runeml).html($(options.runeml).html() + $(options.runeml).html());    //复制一份
+
+        //滚动
+        var run = function () {
+            $(options.runeml).stop().animate({
+                'margin-left': -$(options.runeml).find("li").outerWidth(true)
+            }, 500, function () {
+                $(options.runeml).append($(options.runeml).find('li').first());
+                $(options.runeml).css({'margin-left': 0});
+            });
+
+        };
+        //定时器
+        options.timer = setInterval(run, options.gap);
+
+        $(options.runeml).on('mouseover', function () {
+            clearInterval(options.timer);
+        })
+        $(options.runeml).on('mouseout', function () {
+            options.timer = setInterval(run, options.gap);
+        })
+    }
+})(jQuery)
